f708229d42fb300d0f8bae1150cd46fe72d874da
silvia621 <silvia.amicucci84@gmail.com>
2025-01-29 22:36:11 +0100

    Update esercizio_negozio_animali.py

--- esercizio_negozio_animali.py
+++ esercizio_negozio_animali.py
@@ -9,15 +9,6 @@ while inputCassiere != "-1": #finchè non digito -1 continuo ad aggiungere valor
 nuova_lista_prezzi = [float(prezzi[:-1]) for prezzi in lista_prezzi_animale] #creo una nuova lista con solo i prezzi
 print(nuova_lista_prezzi)  
 
-# for oggetto in lista_prezzi_animale :
-#     if oggetto.endswith(("Y", "y")) and len(lista_prezzi_animale) > 5 :
-#         print ("Hai diritto allo sconto, il totale è " , sum(nuova_lista_prezzi)*0.8 , " euro.")
-   
-# for oggetto in lista_prezzi_animale :
-#     if (not(oggetto.endswith(("Y", "y")))) or len(lista_prezzi_animale) < 6 :
-#         print ("Non hai diritto allo sconto, il totale è " , sum(nuova_lista_prezzi) , " euro.")
-
-
 if any ("y" in oggetti.lower() for oggetti in lista_prezzi_animale) and len(lista_prezzi_animale) > 5 : #controllo se c'è almeno un animale e se ci sono almeno 6 prodotti
     print ("Hai diritto allo sconto, il totale è " , sum(nuova_lista_prezzi)*0.8 , " euro.")
 else:
