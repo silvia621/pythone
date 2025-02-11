c9b55baebcfff390368c3a0b122d2ec1c6ee8d31
silvia621 <silvia.amicucci84@gmail.com>
2025-02-11 09:46:18 +0100

    Update esempio_03.py

--- esempio_03.py
+++ esempio_03.py
@@ -1,7 +1,7 @@
 punti1 = 0 #punti vinti dal giocatore 1
 punti2 = 0 #punti vinti dal giocatore 2
 
-while (punti1 < 5 and punti2 < 5) or abs((punti1 - punti2) < 2) : #continuo finchè un giocatore non fa almeno 4 punti con 2 di vantaggio
+while (punti1 < 4 and punti2 < 4) or abs((punti1 - punti2) < 2) : #continuo finchè un giocatore non fa almeno 4 punti con 2 di vantaggio
     vincitore = int(input("Scrivi il vincitore: "))
     if vincitore == 1 :
         punti1 += 1
